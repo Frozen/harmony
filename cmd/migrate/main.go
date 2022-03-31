4a474650838631099799b8c9217befab8f03c75e
Konstantin <k.potapov@softpro.com>
2022-03-31 12:24:14 +0300

    fix.

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/ethdb/leveldb"
 	"github.com/harmony-one/harmony/internal/shardchain"
 )
@@ -29,12 +30,28 @@ func main() {
 	chainDB, err := shardDb.NewChainDB(0)
 	p(err)
 
+	var batch ethdb.Batch
+
+	i := 0
 	for iter.Next() {
 		// Remember that the contents of the returned slice should not be modified, and
 		// only valid until the next call to Next.
+		if batch == nil {
+			batch = chainDB.NewBatch()
+		}
 		key := iter.Key()
-		err := chainDB.Put(key, iter.Value())
+		err := batch.Put(key, iter.Value())
+		p(err)
 		fmt.Println(string(key))
+		if i > 0 && i%10000 == 0 {
+			err := batch.Write()
+			p(err)
+			batch = nil
+		}
+		i++
+	}
+	if batch != nil {
+		err := batch.Write()
 		p(err)
 	}
 	iter.Release()
